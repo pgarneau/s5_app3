5f0cbb7ebcca5fb99ed888e2e8cd98137763d061
Philippe Garneau <garp2405@usherbrooke.ca>
2018-02-06 15:01:31 -0500

    testing

--- mbed_default/main.cpp
+++ mbed_default/main.cpp
@@ -1,21 +1,27 @@
 #include "mbed.h"
 
 DigitalOut pin8(p8);
+Serial pc(USBTX, USBRX, 9600);
+//DigitalOut pin13(p13);
+Serial xbee(p13, p14, 9600);
 
-Serial serial(p13, p14, 9600);
-
-
-
-
-
-// main() runs in its own thread in the OS
 int main() {
 	pin8 = 0;
-	wait(0.4);
+	Thread::wait(410);
 	pin8 = 1;
 	
     while (true) {
-		printf("allo");
+		if (pc.readable()) {
+			int pc_result = pc.getc();
+			printf("%d\n\r", pc_result);
+			xbee.putc(pc_result);
+		}
+		
+		if (xbee.readable()) {
+			int xbee_result = xbee.getc();
+			printf("%d\n\r", xbee_result);
+			pc.putc(xbee_result);
+		}
     }
 }
 
